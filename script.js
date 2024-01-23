6b756cad1ee4af981fd10755a093f170e9b3d7fd
TarunAika <139627399+TarunAika@users.noreply.github.com>
2024-01-23 23:33:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function showSideBar() {
+    const sideBar = document.querySelector('.sidebar');
+    sideBar.style.display = 'flex';
+}
+
+function hideSideBar() {
+    const hidesidebar = document.querySelector('.sidebar');
+    hidesidebar.style.display = 'none';
+}
+
